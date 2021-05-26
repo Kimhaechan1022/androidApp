dc5580b52a7b1bd75c5f3a5884d6d5a4166715e8
Haechan <78046289+Kimhaechan1022@users.noreply.github.com>
2021-05-26 13:33:27 +0900

    googleMap

---
+++ googleMapmain/java/com/example/myapplication/MapsActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+import androidx.fragment.app.FragmentActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng home = new LatLng(36.361111565474246, 127.34957220639937);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(home);
+        markerOptions.title("궁동");
+        markerOptions.snippet("기점");
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 10));
+    }
+}
